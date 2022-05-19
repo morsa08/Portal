4c82b4b189d44720e7de9c0da2fc754e8602a223
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-05-18 21:17:20 -0500

    Added more accordions

--- server.js
+++ server.js
@@ -2,7 +2,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const https = require('https');
 
-app.get("/", function(request, response){
-  console.log(request);
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// app.get("/", function(req, res){
+//   console.log(request);
+// });
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
 });
