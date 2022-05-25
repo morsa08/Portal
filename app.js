b10cb384a8f116e4bdc47645b6a115ec6bbd0152
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-05-25 01:10:13 -0500

    Testing Server
    Server functions are working on localhost. GET db.json to populate tables, and POST to db.json when creating new ticket.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-if(document.body.contains(document.getElementById("date"))){
-var dateTime = Date();
-document.getElementById("date").textContent = dateTime;
+// jshint esversion:8
+
+// CHECK FOR DATE ELEMENT AND INSERT IF FOUND
+if (document.body.contains(document.getElementById("date"))) {
+  var dateTime = Date();
+  document.getElementById("date").textContent = dateTime;
 }
 
+// ADD EVENT LISTENERS
 var e = document.getElementById("criteria");
-e.addEventListener("change", showDropdown);
+if (e) {
+  e.addEventListener("change", showDropdown);
+}
 
+// DROPOWN FUNCTION
 function showDropdown() {
 
   if (criteria.value > 0) {
@@ -36,4 +43,90 @@ function showDropdown() {
   if (criteria.value == 6) {
     $(".driverSearch").css("display", "none");
   }
+}
+
+// FETCH DATABASE FROM API AND STORE
+const url = "http://localhost:3000/testing";
+if (document.getElementById("openTable")) {
+  fetch(url)
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      appendData(data);
+    })
+    .catch(function(err) {
+      alert("Something's fucky!!");
+      console.log(err);
+
+    });
+}
+
+// INSERT DATA INTO TABLE
+function appendData(data) {
+  for (i = 0; i < data.customers.length; i++) {
+    var table = document.getElementById("openTable");
+    var row = table.insertRow(i + 1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = data.customers[i].ticket_no;
+    cell2.innerHTML = data.customers[i].name;
+    cell3.innerHTML = data.customers[i].street;
+
+  }
+
+}
+
+const customerForm = document.getElementById("customerForm");
+
+if (customerForm) {
+  customerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    let form = e.currentTarget;
+    let url = form.action;
+
+    try {
+      let formData = new FormData(form);
+      let responseData = await postFormFieldsAsJson({
+        url,
+        formData
+      });
+
+      let {
+        serverDataResponse
+      } = responseData;
+      console.log(serverDataResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+}
+async function postFormFieldsAsJson({
+  url,
+  formData
+}) {
+
+  let formDataObject = Object.fromEntries(formData.entries());
+  let formDataJsonString = JSON.stringify(formDataObject);
+
+  let fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: formDataJsonString,
+  };
+
+  let res = await fetch(url, fetchOptions);
+
+  if (!res.ok) {
+    let error = await res.text();
+    throw new Error(error);
+  }
+  return res.json();
 }
--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-// jshint esversion:6
+// jshint esversion: 8
 const express = require('express');
 const app = express();
 const port = 3000;
 const https = require('https');
+const database = require("./db.json");
+const cors = require("cors");
+const fs = require("fs");
+
+app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.urlencoded({
   extended: true
 }));
 
-// app.get("/", function(req, res){
-//   console.log(request);
-// });
+app.get("/testing", function(request, response) {
+  response.json(database);
+});
 
+
+
+
+// POSTING JSON TO SERVER
+
+app.post("/testing", (request, response) => {
+
+let resData = (request.body);
+// console.log(resData);
+
+const jsonString = fs.readFileSync("db.json", "utf8");
+const jsonObject = JSON.parse(jsonString);
+
+// PUSH NEW CUSTOMER INTO ARRAY
+if (jsonObject.customers) {
+jsonObject.customers.push(resData);
+}
+
+
+// CONVERT jsonObject TO JSON AND WRITE TO DB.JSON
+  fs.writeFile("db.json", JSON.stringify(jsonObject, null, 2), (err) => {
+    if (err) {
+      console.log(err);
+    }
+
+    // console.log(resData);
+    response.status(200).json(resData);
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
-});
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  // console.log(database);
 });
