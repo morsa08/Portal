be59c4432e22fee79e9f4fc5e7c3d9898c67278a
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-06-06 22:31:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // jshint esversion: 8
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3001;
 // const port = "https://portal-service-tracker.herokuapp.com/";
 const https = require('https');
 const database = require("./db.json");
