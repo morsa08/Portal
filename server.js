a7091e9295e0c5ba65609cd4c2b8fa8db519863e
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-06-08 19:21:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,20 @@ const app = express();
 const port = 3001;
 // const port = "https://portal-service-tracker.herokuapp.com/";
 const https = require('https');
-const database = require("./db.json");
-const cors = require('cors');
 const fs = require("fs");
-
-// const httpsServer = https.createServer(parameters, app);
+const database = fs.readFile("/db.json");
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
 app.use(express.urlencoded({
   extended: true
 }));
 
-console.log("dookiePoo");
+
+
 
 app.get("/", function(request, response) {
-  // response.header("Access-Control-Allow-Origin", "*");
-  // response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   response.json(database);
 });
 
@@ -38,41 +27,34 @@ app.get("/", function(request, response) {
 // POSTING JSON TO SERVER
 
 app.post("/", (request, response) => {
-// response.header("Access-Control-Allow-Origin", "*");
-// response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  let resData = (request.body);
+  const jsonString = fs.readFileSync("db.json", "utf8");
+  const jsonObject = JSON.parse(jsonString);
 
-let resData = (request.body);
-const jsonString = fs.readFileSync("db.json", "utf8");
-const jsonObject = JSON.parse(jsonString);
-
-// console.log("RESDATA" + resData);
-console.log("Db.json reading:" +jsonString);
-// console.log("JSON OBJECT" + jsonObject);
+  // console.log("RESDATA" + resData);
+  console.log("Db.json reading:" + jsonString);
+  // console.log("JSON OBJECT" + jsonObject);
 
 
-// PUSH NEW CUSTOMER INTO ARRAY
-if (jsonObject.customers) {
-jsonObject.customers.push(resData);
-} else {
-  console.log("Json does not contain customers");
-}
+  // PUSH NEW CUSTOMER INTO ARRAY
+  if (jsonObject.customers) {
+    jsonObject.customers.push(resData);
+  } else {
+    console.log("Json does not contain customers");
+  }
 
 
-// CONVERT jsonObject TO JSON AND WRITE TO DB.JSON
-   fs.writeFile("db.json", JSON.stringify(jsonObject, null, 2), (err) => {
+  // CONVERT jsonObject TO JSON AND WRITE TO DB.JSON
+  fs.writeFile("db.json", JSON.stringify(jsonObject, null, 2), (err) => {
     if (err) {
       console.log(err);
     }
-     });
-
-
-
-     response.status(204).send();
-
+  });
+  response.status(204).send();
 });
 
 
- app.listen(port, function() {
-  console.log("Http server is running on port " + port);
+app.listen(port, function() {
+  console.log("Https server is running on port " + port);
   // console.log(database);
 });
